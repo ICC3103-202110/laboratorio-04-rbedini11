9eaa8150029e4248bcef85458a629ced26c8d4f7
RENZO <rbedini@miuandes.cl>
2021-05-09 22:37:36 -0400

    paso 2

--- lab4.js
+++ lab4.js
@@ -1,7 +1,15 @@
-const root= number => number**0.5;
-const roots =list_numbers =>{
-    return list_numbers.map(root);
+function view(counter){
+    return counter
 }
-arrayOfNumbers= [0,1,2,3,4,5,6,7,8,9];
 
-console.log(roots(arrayOfNumbers));
+function update(msg, counter){
+    if (msg==='+'){
+        return counter + 1
+    }
+    if (msg==='-'){
+        return counter - 1
+    }
+    else{
+        return counter
+    }
+}
