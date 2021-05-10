902558e003aaa3dd850e01efea0ab19c30653271
RENZO <rbedini@miuandes.cl>
2021-05-09 22:28:36 -0400

    paso 1

---
+++ lab4.js
@@ -0,0 +1,7 @@
+const root= number => number**0.5;
+const roots =list_numbers =>{
+    return list_numbers.map(root);
+}
+arrayOfNumbers= [0,1,2,3,4,5,6,7,8,9];
+
+console.log(roots(arrayOfNumbers));
